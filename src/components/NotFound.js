fc315f5102f2148c64e6fa52c37d420967e5f755
hendpraz <mhendryp99@gmail.com>
2019-11-06 23:25:51 +0700

    Add Single Page Application Structure

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h2>404 Not Found</h2>
+                <p>Kembali ke Home</p>
+            </div>
+        );
+    }
+}
+export default NotFound;

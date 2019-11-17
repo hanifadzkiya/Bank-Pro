64e2edb46e74546e3f5ed1190f10e0991d8dc612
hendpraz <mhendryp99@gmail.com>
2019-11-17 21:36:26 +0700

    Add Test and CI/CD configs

---
+++ deploy/updateAndRestart.sh
@@ -0,0 +1,20 @@
+set -e
+
+# Delete the old repo
+sudo su
+rm -rf /home/ubuntu/html/ws-bank/
+sudo su ubuntu
+
+# clone the repo again
+cd /home/ubuntu/html
+git clone git@gitlab.informatika.org:if3110-2019-02-k03-18/bank-pro.git
+
+cd /home/ubuntu/html/bank-pro
+
+#install npm packages
+echo "Running npm install"
+npm install
+
+#Build
+echo "Running npm build"
+npm run build

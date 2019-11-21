41672b3f720e3b88e15b0612504af4fb92da9b59
hanif adzkiya <hanifadzkiya@gmail.com>
2019-11-21 09:18:45 +0700

    Add Login components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,54 @@
 import React, { Component } from "react";
 import {Redirect} from 'react-router-dom';
 import "./submit.css";
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-class Login extends Component {
-    constructor(props){
-        super(props)
-        this.login = this.login.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {
-          no_rekening: 'sw'
-        };
-    }
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-    login(event){
-        // sessionStorage.setItem('authToken', 'testLoginToken');
-        var soap = require('soap');
-        var url = 'http://localhost:9000/wsbank?wsdl';
-        var args = {arg0: '089649993811'};
-        soap.createClient(url, function(err, client) {
-          client.isRekeningExist(args, function(err, result) {
-              alert(result["return"]);
-          });
-        });
-        event.preventDefault()
-        // alert(this.state.no_rekening);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event){
-        console.log(event);
-        this.setState({no_rekening: event.target.value});
-    }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    console.log(event.target.value);
+  }
 
-    render() {
-        return (
-            <div style={{textAlign: "center"}}>
-                <h2>Bank Pro Login</h2>
-                <form onSubmit = {this.login}>
-                    <input id="input-rekening" type="number" value={this.state.no_rekening} onChange={this.handleChange}/>
-                    <input id="submit" type="submit" value="Login"/>
+  handleSubmit(event) {
+    var soap = require('soap');
+    var url = 'https://cors-anywhere.herokuapp.com/3.1.12.44:8080/ws-bank_war/services/wsbank?wsdl';
+    var args = {arg0: '089649993811'};
+    soap.createClient(url, function(err, client) {
+        console.log(client);
+      client.isRekeningExist(args, function(err, result) {
+          alert(result["return"]);
+      });
+    });
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <Row>
+            <Col sm={4} className="left-side">
+                <div className="welcome">
+                    <h2>BankPro</h2>
+                </div>
+            </Col>
+            <Col sm={8}>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                      <input className="input-rekening" type="number" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input className="submit-rekening" type="submit" value="Login"/>
                 </form>
-            </div>
-        );
-    }
+            </Col>
+        </Row>
+    );
+  }
 }
 export default Login;
